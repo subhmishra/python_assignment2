2683a1f620c3c1cdd587d31763bcfdc6f1324b0e
Shubham Mishra <83873389+subhmishra@users.noreply.github.com>
2025-04-20 03:14:30 +0530

    Add files via upload

---
+++ task0001.py
@@ -0,0 +1,12 @@
+                             # Task 1: Check if a Number is Even or Odd
+
+num1 = int(input("enter a number:"))
+if num1 % 2 == 0:
+ print(+num1," is an even number")
+else:
+    print(+num1,"is an  odd number")
+
+
+# output
+# enter a number:50
+# 50  is an even number
---
+++ task0002.py
@@ -0,0 +1,13 @@
+                    # Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+total =0
+numb = range(1 ,51)
+
+for num in numb:
+    total += num
+print("sum of numbers from 1 to 50 is : " ,total)
+
+
+
+# output
+# sum of numbers from 1 to 50 is :  1275
